2628cf624d9bcdf890b634b64cbc4ac3aa526082
gharbin <nawres.gharbi@st.com>
2014-10-23 16:28:08 +0100

    update HID application

--- Validation_Projects/USBD_HID/Project/Validation/Application/HID_Application.c
+++ Validation_Projects/USBD_HID/Project/Validation/Application/HID_Application.c
@@ -8,39 +8,7 @@
   ******************************************************************************
 */
 
-/* Boards define--------------------------------------------------------------*/
-//#define stm32f4xx_eval
-//#define stm32f2xx_eval
 
-
-
-
-/* BSP Includes --------------------------------------------------------------*/
-//#ifdef stm32f4xx_eval
-//  #include "stm324x9i_eval.c" 
-//  #include "stm324x9i_eval_io.c" 
-//  #include "stm324x9i_eval.h" 
-//  #include "stm324x9i_eval_io.h" 
-//  #include "stmpe1600.c"
-//  #include "../../Drivers\STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h"
-//  #include "../../Drivers\STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c"
-//  #include "../../Drivers\STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c"
-//  #include "../../Drivers\STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h"
-//  #include "../../Drivers\STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c"
-//#endif
-//
-//#ifdef stm32f2xx_eval
-//  #include "stm322xg_eval.c" 
-//  #include "stm322xg_eval_io.c" 
-//  #include "stm322xg_eval.h" 
-//  #include "stm322xg_eval_io.h" 
-//  #include "stmpe811.c"
-//  #include "../../Drivers/STM32F2xx_HAL_Driver/Inc/stm32f2xx_hal_i2c.h"
-//  #include "../../Drivers\STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_i2c.c"
-//  #include "../../Drivers\STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_i2c_ex.c"
-//  #include "../../Drivers\STM32F2xx_HAL_Driver/Inc/stm32f2xx_hal_uart.h"
-//  #include "../../Drivers\STM32F2xx_HAL_Driver/Src/stm32f2xx_hal_uart.c"
-//#endif
 
 
 /* Includes ------------------------------------------------------------------*/
@@ -50,50 +18,66 @@
 /* Private typedef -----------------------------------------------------------*/
 extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 extern USBD_HandleTypeDef hUsbDeviceFS;
-extern uint32_t HID_Transfert;
 /* Private define ------------------------------------------------------------*/
-#define CURSOR_STEP     5
+#define CURSOR_STEP     1
 uint32_t HID_Transfert = 0;
-uint32_t counter_x = 0;
-uint32_t counter_y = 0;
-int i,j = 0;
-
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-uint8_t HID_Buffer[4];
 /* Private function prototypes -----------------------------------------------*/
-void Toggle_Leds(void);
-void HIDApplication_Start(void);
-static void GetPointerDatax(uint8_t *pbuf);
-static void GetPointerDatay(uint8_t *pbuf);
+static void GetPointerData(uint8_t *pbuf);
 /* Private functions ---------------------------------------------------------*/
-
-void HIDApplication_Start()
-{
-  /* begin transfert of HID packets to the host */
-  HID_Transfert = 1;
-}
-
 /* Get joystick state---------------------------------------------------------*/
+
+
+
+
+
+
+/* Application ---------------------------------------------------------------*/
 /**
   * @brief  Gets Pointer Data.
   * @param  pbuf: Pointer to report
   * @retval None
   */
-static void GetPointerDatax(uint8_t *pbuf)
+static void GetPointerData(uint8_t *pbuf)
 {
-  static int8_t cnt = 0;
-  int8_t  x = 0, y = 0 ;
   
-  if(cnt++ > 0)
+  static int8_t x = 0;
+  static int8_t y = 0;
+  static int8_t Sens = 0;
+  static int8_t Pas = 0;
+  
+  if (Pas == 20)
   {
-    x = CURSOR_STEP;
-    y = CURSOR_STEP;
+    Pas=0;
+    Sens++;
   }
-  else
+  
+  if(Sens == 0)
   {
-    x = -CURSOR_STEP;
-    y = -CURSOR_STEP;
+    x=Pas++;
+    y=0;       
+  } 
+  if(Sens == 1)
+  {
+    y=Pas++;
+    x=0;   
+  } 
+  if (Sens == 2)
+  {
+    x=256-Pas++;
+    y=0;
+  } 
+  if (Sens == 3)
+  {
+    y=256-Pas++;
+    x=0;
+  } 
+  if (Sens == 4)
+  { 
+    Sens=0;
+    x=0;
+    y=0; 
   }
   
   pbuf[0] = 0;
@@ -102,56 +86,27 @@ static void GetPointerDatax(uint8_t *pbuf)
   pbuf[3] = 0;
 }
 
-static void GetPointerDatay(uint8_t *pbuf)
-{
-  static int8_t cnt = 0;
-  int8_t  x = 0, y = 0 ;
-  
-  if(cnt++ > 0)
-  {
-    y = CURSOR_STEP;
-  }
-  else
-  {
-    y = -CURSOR_STEP;
-  }
-  
-  pbuf[0] = 0;
-  pbuf[1] = x;
-  pbuf[2] = y;
-  pbuf[3] = 0;
-}
 
 /* HID send command with systick callback-------------------------------------*/
 void HAL_SYSTICK_Callback(void)
 {
-uint8_t HID_Buffer[4];
+  uint8_t HID_Buffer[4];
   static __IO uint32_t counter=0;
 
   
   /* check Joystick state every 10ms */
   if (counter++ == 10)
   {  
-    GetPointerDatax(HID_Buffer);
+    GetPointerData(HID_Buffer);
     
-    
-    /* send data though IN endpoint*/
-    if((HID_Buffer[1] != 0) || (HID_Buffer[2] != 0))
-    {
-      USBD_HID_SendReport(&hUsbDeviceFS, HID_Buffer, 4);
-    }
-    
-    GetPointerDatay(HID_Buffer);
     
     /* send data though IN endpoint*/
     if((HID_Buffer[1] != 0) || (HID_Buffer[2] != 0))
     {
       USBD_HID_SendReport(&hUsbDeviceFS, HID_Buffer, 4);
     }    
-    
     counter =0;
   }
 
 }
-
 /* ---------------------------------------------------------------------------*/
